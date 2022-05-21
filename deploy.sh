8d0838dd4b00e0f0728bd3f60805151cb37e2dba
David Fölk <foelkdavid@actscale.xyz>
2022-05-21 15:44:22 +0200

    fixed

--- deploy.sh
+++ deploy.sh
@@ -14,19 +14,20 @@ mkdir -p $HOME/.config/zsh
 
 #copy
 cp -f $PWD/dunst/dunstrc $HOME/.config/dunst/
-cp -f $PWD/dwm/config.h $HOME/.local/usr/src/dwm/
-cp -f $PWD/dwmblocks/blocks.h $HOME/.local/usr/src/dwmblocks/
-cp -rf $PWD/dwmblocks/modules/ $HOME/.local/usr/src/dwmblocks/
+cp -f $PWD/dwm/config.h $HOME/.local/src/dwm/
+cp -f $PWD/dwmblocks/blocks.h $HOME/.local/src/dwmblocks/
+cp -rf $PWD/dwmblocks/modules/ $HOME/.local/src/dwmblocks/
 cp -f $PWD/neofetch/config.conf $HOME/.config/neofetch/
-cp -f $PWD/nsxiv/config.h $HOME/.local/usr/src/nsxiv/
+cp -f $PWD/nsxiv/config.h $HOME/.local/src/nsxiv/
 cp -f $PWD/picom/picom.conf $HOME/.config/picom/
 cp -rf $PWD/rofi/themes/* $HOME/.config/rofi/
-cp -f $PWD/st/config.h $HOME/.local/usr/src/st/
+cp -f $PWD/st/config.h $HOME/.local/src/st/
 cp -f $PWD/x/xinitrc $HOME/.config/x/
 cp -f $PWD/zsh/.zshrc $HOME/.config/zsh/
 
 #link
 mkdir dwm dwmblocks st nsxiv
+rm dwm/config.h st/config.h dwm/blocks.h nsxiv/config.h
 ln -s $HOME.local/src/dwm/config.h dwm/config.h
 ln -s $HOME.local/src/st/config.h st/config.h
 ln -s $HOME.local/src/dwmblocks/blocks.h dwm/blocks.h
