5a5bf94e9e1451c4a86d4d8ebe37ee8f7119318d
David Fölk <foelkdavid@actscale.xyz>
2022-05-22 18:32:13 +0200

    fixed warnings

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,9 @@ mkdir -p $HOME/.config/st
 mkdir -p $HOME/.config/x
 mkdir -p $HOME/.config/zsh
 
+
+#rm $HOME/.config/dwm/config.h $HOME/.config/st/config.h $HOME/.config/dwmblocks/blocks.h $HOME/.config/nsxiv/config.h
+
 #copy
 cp -f $PWD/dunst/dunstrc $HOME/.config/dunst/
 cp -f $PWD/dwm/config.h $HOME/.local/src/dwm/
@@ -22,17 +25,17 @@ cp -f $PWD/nsxiv/config.h $HOME/.local/src/nsxiv/
 cp -f $PWD/picom/picom.conf $HOME/.config/picom/
 cp -rf $PWD/rofi/themes $HOME/.config/rofi/
 cp -f $PWD/st/config.h $HOME/.local/src/st/
-cp -f $PWD/x/xinitrc $HOME/.config/x/
-cp -f $PWD/zsh/.zshrc $HOME/.config/zsh/
+#cp -f $PWD/x/xinitrc $HOME/.config/x/
+cp -f $PWD/zsh/zshrc $HOME/.config/zsh/
 
 #link
-mkdir dwm dwmblocks st nsxiv
-rm dwm/config.h st/config.h dwm/blocks.h $HOME/.config/nsxiv/config.h
-ln -s $HOME.local/src/dwm/config.h $HOME/.config/dwm/config.h
-cd $HOME.local/src/dwm && make && sudo make install
-ln -s $HOME.local/src/st/config.h $HOME/.config/st/config.h
-cd $HOME.local/src/st && make && sudo make install
-ln -s $HOME.local/src/dwmblocks/blocks.h $HOME/.config/dwmblocks/blocks.h
-cd $HOME.local/src/dwmblock && make && sudo make install
-ln -s $HOME.local/src/nsxiv/config.h $HOME/.config/nsxiv/config.h
-cd $HOME.local/src/nsxiv && make && sudo make install
+#mkdir dwm dwmblocks st nsxiv
+
+ln -s $HOME/.local/src/dwm/config.h $HOME/.config/dwm/config.h
+cd $HOME/.local/src/dwm && make && sudo make install
+ln -s $HOME/.local/src/st/config.h $HOME/.config/st/config.h
+cd $HOME/.local/src/st && make && sudo make install
+ln -s $HOME/.local/src/dwmblocks/blocks.h $HOME/.config/dwmblocks/blocks.h
+cd $HOME/.local/src/dwmblock && make && sudo make install
+ln -s $HOME/.local/src/nsxiv/config.h $HOME/.config/nsxiv/config.h
+cd $HOME/.local/src/nsxiv && make && sudo make install
